752b5405f8f41a3325b79ae0c1d58e9226d144b5
jacobcies <48026304+jacobcies@users.noreply.github.com>
2022-04-28 15:25:56 +0100

    Add files via upload

---
+++ lang/lang.en.php
@@ -0,0 +1,124 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+// Index
+$lang['createphotoalbum'] = 'Create Photo Album';
+$lang['uploadphotos']= 'Upload Photos';
+$lang['managephotos']= 'Manage Photos and Albums';
+$lang['sharephotos']= 'Share to Friends';
+$lang['newalbum'] = 'New Album';
+$lang['gotoalbums']= 'Go to Albums';
+
+$lang['footermain'] = 'We will <span class="fw-bolder d-inline-block pb-1 position-relative text-danger" style="color: #fb2905 !important;">Resize 
+                </span> your photos';
+$lang['footerbody'] = 'For better and faster operation of the website, all photos will be changed in resolution. After changing, each photo on our website will have height / width changed to 1088px.';
+
+// Menu
+
+$lang['home'] = 'Home';
+$lang['albums'] = 'Albums';
+$lang['videos'] = 'Videos';
+$lang['all'] = 'All';
+$lang['favourites'] = 'Favourites';
+$lang['profile'] = 'Profile';
+$lang['signout'] = 'Sign Out';
+$lang['storage'] = 'Storage';
+$lang['basket'] = 'Basket';
+$lang['comments'] = 'Comments';
+$lang['contact'] ='Contact';
+
+$lang['share'] = 'Share';
+
+$lang['latestalbums'] = 'Latest Albums';
+$lang['latestvideos'] = 'Latest Videos';
+$lang['random'] = 'Random Pictures';
+
+//Search
+
+$lang['search'] = 'Search';
+$lang['lookingfor'] = 'What you are looking for?';
+
+//photos
+
+$lang['items'] = 'items';
+$lang['more'] = 'View All';
+$lang['map'] = 'Map';
+
+//photos modals
+
+$lang['uploadphotos'] = 'Upload Photos';
+$lang['droptoupload']= 'Drop images to upload';
+$lang['clicktopick']= 'or click to pick manually';
+
+
+//profile
+
+$lang['guest'] = 'Guest';
+$lang['administrator'] = 'Administrator';
+$lang['change'] = 'Change';
+$lang['upuser'] = 'Update User';
+$lang['uploadphoto'] = 'Upload your Photo';
+$lang['deleteaccount'] = 'Delete Account';
+$lang ['changename'] = 'Change your Name';
+$lang['changepassword'] = 'Change your Password';
+$lang['changelang'] = 'Change Language';
+
+//profile modal update user
+
+$lang['fname'] = 'First Name';
+$lang['sname'] = 'Last Name';
+$lang['passwd'] = 'Password';
+$lang['language'] = 'Language';
+$lang['close'] = 'Close';
+$lang['submit'] = 'Submit';
+
+//blog
+
+$lang['back'] = 'Back';
+
+// video breadcrumbs
+
+$lang['allvideos'] = 'All Videos';
+$lang['videos'] = 'Videos';
+
+// albums breadcrumbs
+
+$lang['allalbums'] = 'All Albums';
+$lang['albums'] = 'Albums';
+
+//userbasket
+$lang['savebasket'] = 'Save Basket';
+$lang['flushbasket'] = 'Flush Basket';
+$lang['newbasket'] = 'New Basket Name';
+$lang['basketexist'] = 'Add to existing basket';
+$lang['update'] = 'Update';
+$lang['deletebasket'] = 'Delete Basket';
+$lang['delete'] = 'Delete';
+$lang['donotdelete'] = 'Do not delete';
+$lang['dontflush'] = 'Do not flush';
+$lang['downloadbasket'] = 'Download Basket';
+
+//lastcomm
+
+$lang['latestcom'] = 'Latest Comments';
+$lang['photo'] = 'Photo';
+$lang['when'] = 'When';
+$lang['user'] = 'User';
+$lang['comment'] = 'Comment';
+
+// comments
+
+$lang['imagesize'] = 'Image Size';
+$lang['imagename']= 'Image Name';
+$lang['albumname'] = 'Album Name';
+$lang['albumurl']= 'Album URL';
+$lang['imagemade'] = 'Image Made';
+$lang['entercomment']= 'Enter Comment';
+
+?>
---
+++ lang/lang.pl.php
@@ -0,0 +1,123 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+// Index
+$lang['createphotoalbum'] = 'Twórz Foto Albumy';
+$lang['uploadphotos']= 'Dodawaj Zdjęcia';
+$lang['managephotos']= 'Zarządzaj Albumami';
+$lang['sharephotos']= 'Udostępniaj znajomym';
+$lang['newalbum'] = 'Nowy Album';
+$lang['gotoalbums']= 'Przejdź do Albumów';
+
+$lang['footermain'] = 'Zmienimy <span class="fw-bolder d-inline-block pb-1 position-relative text-danger" style="color: #fb2905 !important;">Rozmiar 
+                </span> Twoich zdjęć';
+$lang['footerbody'] = 'Dla lepszego i szybszego działania strony, wszystkie zdjęcia zostaną zmienione w rozdzielczości. Po zmianie, każde zdjęcie na naszej stronie będzie miało zmienioną wysokość/szerokość na 1088px.';
+
+
+// Menu
+
+$lang['home'] = 'Główna';
+$lang['albums'] = 'Albumy';
+$lang['videos'] = 'Filmy';
+$lang['all'] = 'Wszystkie';
+$lang['favourites'] = 'Ulubione';
+$lang['profile'] = 'Profil';
+$lang['signout'] = 'Wyloguj';
+$lang['storage'] = 'Dysk';
+$lang['basket'] = 'Koszyk';
+$lang['comments'] = 'Komentarze';
+$lang['contact'] ='Kontakt';
+
+$lang['share'] = 'Udostępnij';
+
+$lang['latestalbums'] = 'Ostatnio dodane albumy';
+$lang['latestvideos'] = 'Ostatnio dodane filmy';
+$lang['random'] = 'Losowo wybrane zdjęcia';
+
+//Search
+
+$lang['search'] = 'Szukaj';
+$lang['lookingfor'] = 'Co chcesz znaleźć?';
+
+//photos
+
+$lang['items'] = 'zdjęć';
+$lang['more'] = 'Zobacz wszystkie';
+$lang['map'] = 'Mapa';
+
+//photos modals
+
+$lang['uploadphotos'] = 'Załaduj zdjęcia';
+$lang['droptoupload']= 'Upuść zdjęcia do przesłania';
+$lang['clicktopick']= 'lub kliknij i pobierz manualnie';
+
+//profile
+
+$lang['guest'] = 'Gość';
+$lang['administrator'] = 'Administrator';
+$lang['change'] = 'Zmień';
+$lang['upuser'] = 'Aktualizuj dane';
+$lang['uploadphoto'] = 'Prześlij swoje zdjęcie';
+$lang['deleteaccount'] = 'Usuń konto';
+$lang ['changename'] = 'Zmień swoje Imię i Nazwisko';
+$lang['changepassword'] = 'Zmień swoje hasło';
+$lang['changelang'] = 'Zmień język';
+
+//profile modal update user
+
+$lang['fname'] = 'Imię';
+$lang['sname'] = 'Nazwisko';
+$lang['passwd'] = 'Hasło';
+$lang['language'] = 'Wybierz język';
+$lang['close'] = 'Zamknij';
+$lang['submit'] = 'Zapisz';
+
+//blog
+
+$lang['back'] = 'Wróć';
+
+// video breadcrumbs
+
+$lang['allvideos'] = 'Wszystkie Filmy';
+$lang['videos'] = 'Filmy';
+
+// albums breadcrumbs
+
+$lang['allalbums'] = 'Wszystkie Albumy';
+$lang['albums'] = 'Albumy';
+
+//userbasket
+$lang['savebasket'] = 'Zapisz Koszyk';
+$lang['flushbasket'] = 'Wyczyść koszyk';
+$lang['newbasket'] = 'Stwórz nowy koszyk';
+$lang['basketexist'] = 'Dodaj do istniejącego';
+$lang['update'] = 'Aktualizuj';
+$lang['deletebasket'] = 'Usuń Koszyk';
+$lang['delete'] = 'Usuń';
+$lang['donotdelete'] = 'Nie usuwaj';
+$lang['dontflush'] = 'Nie czyść koszyka';
+$lang['downloadbasket'] = 'Pobierz Koszyk';
+
+//lastcomm
+
+$lang['latestcom'] = 'Ostatnie komentarze';
+$lang['photo'] = 'Zdjęcie';
+$lang['when'] = 'Kiedy dodane';
+$lang['user'] = 'Użytkownik';
+$lang['comment'] = 'Komentarz';
+
+// comments
+
+$lang['imagesize'] = 'Rozmiar zdjęcia';
+$lang['imagename']= 'Nazwa zdjęcia';
+$lang['albumname'] = 'Nazwa albumu';
+$lang['albumurl']= 'Link do albumu';
+$lang['imagemade'] = 'Zdjęcie wykonane';
+$lang['entercomment']= 'Dodaj komentarz';
+?>
